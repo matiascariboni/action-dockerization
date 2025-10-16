bf7945443680194cee248ed80b6b2e2a6d806313
Matías Cariboni <mcariboni@icloud.com>
2025-10-16 18:38:09 +0200

    changed some comments

--- scripts/docker_config.sh
+++ scripts/docker_config.sh
@@ -13,14 +13,14 @@ else
   echo "DOCKER_TAG=$(git rev-parse --short HEAD)" >> $GITHUB_OUTPUT
 fi
 
-# Si ENV_NAME está vacío, usar el último segmento del nombre de la rama
+# If ENV_NAME is empty, use the last segment of the branch name
 if [ -z "$ENV_NAME" ]; then
   # Extraer el último segmento después del último "/"
   ENV_NAME=$(echo "${BRANCH_NAME}" | awk -F'/' '{print $NF}')
   echo "ENV_NAME not provided, using branch name: $ENV_NAME"
 fi
 
-# Buscar Dockerfile específico del entorno
+# Search for the environment-specific Dockerfile
 DOCKERFILE_PATH="./Dockerfile.$(echo "${ENV_NAME}" | tr '[:upper:]' '[:lower:]')"
 if [[ -f "$DOCKERFILE_PATH" ]]; then
   echo "Environment-specific Dockerfile found: $DOCKERFILE_PATH"
@@ -30,10 +30,10 @@ else
 fi
 echo "DOCKERFILE_PATH=$DOCKERFILE_PATH" >> $GITHUB_OUTPUT
 
-# Generar nombre del compose file
+# Generate the name of the compose file
 COMPOSE_FILE_NAME="${COMPOSE_NAME}_$(echo "${ENV_NAME}" | tr '[:upper:]' '[:lower:]').yml"
 echo "COMPOSE_FILE_NAME=$COMPOSE_FILE_NAME" >> $GITHUB_OUTPUT
 
-# Generar nombre de imagen
+# Generate image name
 IMAGE_NAME="${GITHUB_REPOSITORY#*/}_${ENV_NAME}"
 echo "IMAGE_NAME=$IMAGE_NAME" >> $GITHUB_OUTPUT
